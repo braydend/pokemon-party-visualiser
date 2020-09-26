07ba8c96436a0918175d45211531eb6faca15a30
Brayden <braydendiluca@gmail.com>
2020-09-26 20:13:46 +1000

    Update <PokeAdder /> to use new API types

--- src/components/PokemonAdder.tsx
+++ src/components/PokemonAdder.tsx
@@ -1,4 +1,5 @@
-import React, { createRef, useRef, useState } from 'react';
+import React, { createRef, useState } from 'react';
+import { PokeAPIPokemon } from '../api-types';
 import { PokemonType } from './Pokemon';
 
 type Props = {
@@ -8,39 +9,49 @@ type Props = {
 const PokemonAdder: React.FC<Props> = ({ onAddToParty }) => {
     const endpoint = 'https://pokeapi.co/api/v2/pokemon/';
     const searchBarRef = createRef<HTMLInputElement>();
-    const [pokemonSprite, setPokemonSprite] = useState<string>('');
-    const [pokemonName, setPokemonName] = useState<string>('');
+    const [pokemonData, setPokemonData] = useState<PokeAPIPokemon>();
+    const [nickname, setNickname] = useState<string>();
     const [isLoading, setIsLoading] = useState(false);
+
+    const clearData = () => {
+        setPokemonData(undefined);
+        setNickname(undefined)
+    };
 
     const handleSearch = async () => {
         setIsLoading(true);
+        
         const searchTerm = searchBarRef.current?.value;
-        const repsonse = await (await fetch(`${endpoint}${searchTerm}`)).json();
+        const repsonse: PokeAPIPokemon = await (await fetch(`${endpoint}${searchTerm}`)).json();
 
-        const sprite = repsonse.sprites.front_default;
-        setPokemonSprite(sprite);
+        setPokemonData(repsonse);
         setIsLoading(false);
     };
 
     const handleAddToParty = () => {
+        if (!pokemonData){
+            throw new Error('Cannot add a pokemon to your party without an API response');
+        }
+
         const pokemon: PokemonType = {
-            name: pokemonName,
-            sprite: pokemonSprite,
+            name: nickname || pokemonData.name,
+            sprite: pokemonData.sprites.front_default,
         };
 
         onAddToParty(pokemon);
+        clearData();
     };
 
-    const isButtonDisabled = !pokemonSprite || !pokemonName || isLoading;
+    const isButtonDisabled = !pokemonData || isLoading;
 
     return (
         <div>
             <div>
-                <input ref={searchBarRef} placeholder="Pokemon to add to party" />
-                <button onClick={() => handleSearch()}>Serach</button>
+                <input ref={searchBarRef} placeholder="Search for a pokemon" />
+                <button onClick={() => handleSearch()}>Search</button>
             </div>
             <div>
-                <input placeholder="Nickname for pokemon" onChange={({ target: { value } }) => setPokemonName(value)} />
+                <input placeholder="Nickname" onChange={({ target: { value } }) => setNickname(value)} />
                 <button onClick={handleAddToParty} disabled={isButtonDisabled}>Add to party</button>
             </div>
         </div>
